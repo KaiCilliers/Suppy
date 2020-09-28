aec45dc006c7e7e2e4d8db2951431866026dac36
Kai Cilliers <kaijohan428@gmail.com>
2020-09-28 11:21:37 +0100

    initial commit

---
+++ app/src/main/java/com/example/suppy/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.suppy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(
+            Intent(
+                applicationContext,
+                MainActivity::class.java
+            )
+        )
+        finish()
+    }
+}
