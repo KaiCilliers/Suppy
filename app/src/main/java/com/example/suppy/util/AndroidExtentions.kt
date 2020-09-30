8a8c4484c03bb6d36cbd4c13e78a92e7b6ec9ed9
Kai Cilliers <kaijohan428@gmail.com>
2020-09-30 13:08:43 +0100

    new bundle extension

--- app/src/main/java/com/example/suppy/util/AndroidExtentions.kt
+++ app/src/main/java/com/example/suppy/util/AndroidExtentions.kt
@@ -1,8 +1,11 @@
 package com.example.suppy.util
 
+import android.os.Bundle
+import android.os.Parcelable
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import java.io.Serializable
 
 /**
  * @desc Subscribe to [LiveData]
@@ -19,3 +22,26 @@ inline fun LiveData<Boolean>.subscribeToNavigation(
         resetBool()
     }
 })
+
+/**
+ * @desc Shorter way of adding bundle data
+ */
+inline fun <T> Bundle.put(key: String, value: T) {
+    when(value) {
+        is Boolean -> putBoolean(key, value)
+        is String -> putString(key, value)
+        is Int -> putInt(key, value)
+        is Short -> putShort(key, value)
+        is Long -> putLong(key, value)
+        is Byte -> putByte(key, value)
+        is ByteArray -> putByteArray(key, value)
+        is Char -> putChar(key, value)
+        is CharArray -> putCharArray(key, value)
+        is CharSequence -> putCharSequence(key, value)
+        is Float -> putFloat(key, value)
+        is Bundle -> putBundle(key, value)
+        is Parcelable -> putParcelable(key, value)
+        is Serializable -> putSerializable(key, value)
+        else -> throw IllegalStateException("Type of property $key is not supported")
+    }
+}
