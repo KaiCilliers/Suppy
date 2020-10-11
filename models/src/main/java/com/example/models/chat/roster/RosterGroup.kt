9528da88860f245d36a3616fac9aaac9264bb350
Kai Cilliers <kaijohan428@gmail.com>
2020-10-11 12:00:56 +0100

    minot comments

--- models/src/main/java/com/example/models/chat/roster/RosterGroup.kt
+++ models/src/main/java/com/example/models/chat/roster/RosterGroup.kt
@@ -1,5 +1,9 @@
 package com.example.models.chat.roster
 
+/**
+ * Represents a single group with
+ * a list of members of that group
+ */
 interface RosterGroup {
     fun add(member: String)
     fun add(vararg members: String)
