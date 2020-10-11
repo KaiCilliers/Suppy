49a1ba98f1e4dc8ddc8da570ee3868b3789c93e1
Kai Cilliers <kaijohan428@gmail.com>
2020-10-11 18:37:13 +0100

    chat frag, adapter, VM cleanup

--- app/src/main/java/com/example/suppy/home/chatlist/ChatsFragment.kt
+++ app/src/main/java/com/example/suppy/home/chatlist/ChatsFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -20,13 +19,11 @@ import com.example.suppy.R
 import com.example.suppy.databinding.FragmentChatsBinding
 import com.example.suppy.util.*
 import kotlinx.android.synthetic.main.fragment_chats.*
-import kotlinx.android.synthetic.main.row_chats.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
-import kotlin.random.Random
 
 /**
  * [Fragment] for UI that displays a user's active
@@ -34,25 +31,22 @@ import kotlin.random.Random
  */
 class ChatsFragment : Fragment() {
 
-    private lateinit var viewModel: ChatsViewModel
-    private lateinit var factory: ChatsViewModelFactory
-    private lateinit var chatAdapter: ChatsAdapter
+    /**
+     * Initialize at first call then afterwards return value
+     */
+    private val chatAdapter by lazy { ChatsAdapter(context = requireContext(), itemClicked = viewModel) }
+    private val viewModel by lazy { ViewModelProvider(this, factory).get(ChatsViewModel::class.java) }
+    private val factory by lazy { ChatsViewModelFactory(chatRepo, msgRepo) }
+    private val chatRepo by lazy { ChatRepo.instance(LocalDatabase.justgetinstance().chatDao()) }
+    private val msgRepo by lazy { MessageRepo.instance( LocalDatabase.justgetinstance().messageDao()) }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentChatsBinding.inflate(inflater)
-        factory = ChatsViewModelFactory(
-            ChatRepo.instance(LocalDatabase.justgetinstance().chatDao()),
-            MessageRepo.instance(LocalDatabase.justgetinstance().messageDao())
-        )
-        Timber.d("Called ViewModelProvider.get for ChatsViewModel1")
-        viewModel = ViewModelProvider(this, factory).get(ChatsViewModel::class.java)
         viewModel.apply {
-            navigateToChatMessages.observeEvent(viewLifecycleOwner){
-//                Timber.d("I should navigate...")
-//                snackbar("${viewModel.bundle.chatName} last message was ${viewModel.bundle.description}", requireView())
+            navigateToChatMessages.observeEvent(viewLifecycleOwner) {
                 findNavController().navigate(
                     R.id.action_chatsFragment_to_chatMessagesFragment,
                     bundleOf("chat" to viewModel.bundle.indentification.chatName)
@@ -61,24 +55,21 @@ class ChatsFragment : Fragment() {
             /**
              * Observe all chat data for any changes
              */
-            getAllChatLocalData().observe(viewLifecycleOwner, Observer {data ->
-                Timber.d("inside the observing code...with $data")
+            getAllChatLocalData().subscribe(viewLifecycleOwner) { data ->
                 val updated = data.map {
                     it.asDomain()
                 }
-                Timber.d("Just before adapter call for setData with $updated")
-                chatAdapter.setData(updated)
-            })
+                Timber.v("Updating chats list data with: $updated")
+                chatAdapter.populate(updated)
+            }
             /**
              * Observe any changes to message table to update chat description
              * with latest message
              */
             getLatestMessaageLocalData().subscribe(viewLifecycleOwner) {
-                it?.let {
-                    Timber.d("Message has been received: ${it.recived}")
-                    Timber.d("$it")
-                    if(!it.recived) {
-                        Timber.d("HEY latest message: $it")
+                it.let {
+                    Timber.v("Message has been received(${it.recived}): $it")
+                    if (!it.recived) {
                         snackbar("${it.body} from ${it.fromName}", requireView())
                         viewModel.updateChatWithNewMessage(it)
                     }
@@ -90,164 +81,99 @@ class ChatsFragment : Fragment() {
                 updateChatsUnreceivedCounter(it)
             }
         }
-        setupAdapter()
         binding.apply { viewModel }
-        /**
-         * Display database contents at launch
-         * TODO obviously this is just for debugging
-         */
-        MainScope().launch {
-            withContext(Dispatchers.IO) {
-                Timber.d("Chat table data:")
-                ChatRepo.instance(LocalDatabase.justgetinstance().chatDao()).justChats().forEach {
-                    Timber.d("$it")
-                }
-                val messages = MessageRepo.instance(LocalDatabase.justgetinstance().messageDao()).justMessages()
-                Timber.d("Message table data count: ${messages.size}")
-                Timber.d("Message count from \"gastly\": ${messages.count { it.fromName == "gastly" }} " +
-                        "of which ${messages.count { it.fromName == "gastly" && it.recived }} are recieved and " +
-                        "${messages.count { it.fromName == "gastly" && !it.recived }} have not been received")
-                Timber.d("Message count from \"weedle\": ${messages.count { it.fromName == "weedle" }} " +
-                        "of which ${messages.count { it.fromName == "weedle" && it.recived }} are recieved and " +
-                        "${messages.count { it.fromName == "weedle" && !it.recived }} have not been received")
-                Timber.d("Message count from \"magikarp\": ${messages.count { it.fromName == "magikarp" }} " +
-                        "of which ${messages.count { it.fromName == "magikarp" && it.recived }} are recieved and " +
-                        "${messages.count { it.fromName == "magikarp" && !it.recived }} have not been received")
-                // This printout can get a bit much
-//                MessageRepo.instance(LocalDatabase.justgetinstance().messageDao()).justMessages().forEach {
-//                    Timber.d("${it.timestamp} - ${it.counter} - received: ${it.recived} - ${it.body}")
-//                }
-                val totalUnrecevied = viewModel.allUnReceived()
-                Timber.d("All unreceived messages: ${totalUnrecevied.size}")
-                Timber.d("weedle unreceived messages: ${totalUnrecevied.count { it.fromName == "weedle" }}")
-                Timber.d("gastly unreceived messages: ${totalUnrecevied.count { it.fromName == "gastly" }}")
-                Timber.d("magikarp unreceived messages: ${totalUnrecevied.count { it.fromName == "magikarp" }}")
-            }
-        }
+        debuggingPrintouts()
         return binding.root
-    }
-
-    private fun setupAdapter(){
-        Timber.d("Setup adapter called... where the adapter is initialised with context and VM")
-        chatAdapter = ChatsAdapter(
-            context = requireContext(),
-            itemClicked = viewModel
-        )
-    }
-
-    /**
-     * Delete a random record from chat database
-     * based on the random records inserted by
-     * user button clicks
-     * TODO temp
-     */
-    private fun deleteARecord() {
-        viewModel.deleteRecord()
-    }
-
-    /**
-     * Fetch all chats from database
-     * TODO temp
-     */
-    private fun localRecords() {
-        viewModel.localRecords()
-    }
-
-    /**
-     * Temporary testing method on button click
-     * to add a new random record to recyclerview
-     * by adding a new record to the database
-     * TODO temp
-     */
-    private fun insertChatFrag(v: View) {
-        viewModel.insertRandomChat()
-    }
-
-    /**
-     * Build and send a message stanza to static recipient
-     * TODO temp
-     */
-    private fun sendMsgStanza() {
-        viewModel.sendMsgStanza(
-            "I am Mr${Random.nextBytes(99999)}",
-            "gastly"
-        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Button onclicks for debugging
-        btn_add_new_item.setOnClickListener{
-            Timber.d("button click")
-            insertChatFrag(it)
-        }
-        btn_fetch_db_records.onClick {
-            Timber.d("Fetch data button clicked...")
-            localRecords()
-        }
-        btn_delete_record.onClick {
-            Timber.d("Delete record button clicked...")
-            deleteARecord()
-        }
-        btn_send_msg_stanza.onClick {
-            Timber.d("Clicked trigger button...")
-            sendMsgStanza()
-        }
-        // Hide these buttons that are mainly used for debugging
-        btn_add_new_item.gone()
-        btn_delete_record.gone()
-        btn_fetch_db_records.gone()
-        btn_send_msg_stanza.gone()
-
-        /**
-         * Add functionality to swipe in the recyclerview
-         * to delete that item
-         * TODO extract this logic to separate class
-         */
-        val itemInteraction = ItemTouchHelper(
-            object : ItemTouchHelper.SimpleCallback(
-                0,
-                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-            ) {
-                override fun onMove(
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    target: RecyclerView.ViewHolder
-                ): Boolean {
-                    return false
-                }
-
-                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                    val position = viewHolder.adapterPosition
-                    val item = chatAdapter.itemAtPosition(position)
-                    snackbar("To delete: $item", requireView())
-                    viewModel.deleteByName(item.indentification.chatName)
-                }
-            }
-        )
-        setupRecyclerView(itemInteraction)
+        uiSetup()
     }
 
     /**
      * Configures the recyclerview called in
      * override fun [onViewCreated]
      */
-    private fun setupRecyclerView(touch: ItemTouchHelper) {
-        Timber.d("setting up RC...")
+    private fun uiSetup() {
+        btn_send_msg_stanza.gone()
+        btn_fetch_db_records.gone()
+        btn_delete_record.gone()
+        btn_add_new_item.gone()
         // For efficiency
         rc_chats.setHasFixedSize(true)
         rc_chats.setItemViewCacheSize(20)
-
         val layoutManager = LinearLayoutManager(context)
         val dividerItemDecoration = DividerItemDecoration(
             rc_chats.getContext(),
             layoutManager.getOrientation()
         )
         rc_chats.addItemDecoration(dividerItemDecoration)
-
-        Timber.d("Going to add layout and adapter to RC...")
-        touch.attachToRecyclerView(rc_chats)
+        touchIndication().attachToRecyclerView(rc_chats)
         rc_chats.layoutManager = layoutManager
         rc_chats.adapter = chatAdapter
+    }
+
+    /**
+     * Add functionality to swipe in the recyclerview
+     * to delete that item
+     * TODO extract this logic to separate class
+     */
+    private fun touchIndication() = ItemTouchHelper(
+        object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val item = chatAdapter.item(position)
+                snackbar("To delete: $item", requireView())
+                viewModel.deleteByName(item.indentification.chatName)
+            }
+        }
+    )
+    /**
+     * Display database contents at launch
+     * TODO obviously this is just for debugging
+     */
+    private fun debuggingPrintouts() {
+        MainScope().launch {
+            withContext(Dispatchers.IO) {
+                Timber.v("Chat table data:")
+                ChatRepo.instance(LocalDatabase.justgetinstance().chatDao()).justChats().forEach {
+                    Timber.v("$it")
+                }
+                val messages = MessageRepo.instance(LocalDatabase.justgetinstance().messageDao()).justMessages()
+                Timber.v("Message table data count: ${messages.size}")
+                Timber.v("""
+                    Message count from \"gastly\": ${messages.count { it.fromName == "gastly" }} 
+                    of which ${messages.count { it.fromName == "gastly" && it.recived }} are recieved and 
+                    ${messages.count { it.fromName == "gastly" && !it.recived }} have not been received
+                """.trimIndent())
+                Timber.v("""
+                    Message count from \"weedle\": ${messages.count { it.fromName == "weedle" }} 
+                    of which ${messages.count { it.fromName == "weedle" && it.recived }} are recieved and 
+                    ${messages.count { it.fromName == "weedle" && !it.recived }} have not been received
+                """.trimIndent())
+                Timber.v("""
+                    Message count from \"magikarp\": ${messages.count { it.fromName == "magikarp" }} 
+                    of which ${messages.count { it.fromName == "magikarp" && it.recived }} are recieved and 
+                    ${messages.count { it.fromName == "magikarp" && !it.recived }} have not been received
+                """.trimIndent())
+                val totalUnrecevied = viewModel.allUnReceived()
+                Timber.v("All unreceived messages: ${totalUnrecevied.size}")
+                Timber.v("weedle unreceived messages: ${totalUnrecevied.count { it.fromName == "weedle" }}")
+                Timber.v("gastly unreceived messages: ${totalUnrecevied.count { it.fromName == "gastly" }}")
+                Timber.v("magikarp unreceived messages: ${totalUnrecevied.count { it.fromName == "magikarp" }}")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/suppy/home/chatlist/ChatsViewModel.kt
+++ app/src/main/java/com/example/suppy/home/chatlist/ChatsViewModel.kt
@@ -3,69 +3,30 @@ package com.example.suppy.home.chatlist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.models.chat.DomainChat
 import com.example.models.chat.EntityChat
 import com.example.models.message.EntityMessage
 import com.example.models.message.UpdatedReceived
 import com.example.repository.ChatRepo
 import com.example.repository.MessageRepo
-import com.example.webservice.Server
 import com.example.suppy.util.VoidEvent
-import com.example.suppy.util.CoroutineContextSource
 import com.example.suppy.util.viewModelIO
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.jivesoftware.smack.chat2.Chat
-import org.jivesoftware.smack.chat2.ChatManager
 import timber.log.Timber
-import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * NOTE use [AndroidViewModel] only if your view model requires context
- * TODO pass repo reference to chatsViewmodel
  * TODO pass coroutine dispatcher here for testing later down the line
- * EXAMPLE
- * priv val repo: Repo,
- * priv val ioDispatcher: CoroutineDispatcher
  */
 class ChatsViewModel(val repo: ChatRepo, val msgRepo: MessageRepo) : ViewModel() {
     lateinit var bundle: DomainChat
-    lateinit var chatMan: ChatManager
-    lateinit var chat: Chat
-    /**
-     * Simply to take note of the viewmodel's
-     * lifecycle awareness aspects
-     * TODO temp
-     */
-    init {
-        Timber.d("ChatsViewModel created!")
-    }
-    override fun onCleared() {
-        super.onCleared()
-        Timber.d("ChatsViewModel destroyed!")
-    }
-
     /**
      * Return all messages that have not been received yet wrapped
      * in LiveData
      */
     fun unReceivedMessagesCounter(): LiveData<List<EntityMessage>> {
-        Timber.d("Fetching all unreceived messages...")
+        Timber.v("Fetching all unreceived messages wrapped in LiveData...")
         return msgRepo.getAllUnreceivedLiveData()
     }
-
-    /**
-     * Used to store randomly generated chat records
-     * to be used to remove those records again on
-     * button click to test recyclerview reaction
-     * to database record deletion
-     * TODO temp
-     */
-    private var addedRecords: ArrayList<EntityChat> = arrayListOf()
-
     /**
      * Standard UI navigation code
      * TODO replace the get with a delegation if possible
@@ -75,202 +36,86 @@ class ChatsViewModel(val repo: ChatRepo, val msgRepo: MessageRepo) : ViewModel()
         get() = _navigateToChatMessages
     fun navigate() {
         _navigateToChatMessages.value = VoidEvent()
-        Timber.d("Chat clicked from VM: $bundle")
     }
-
     /**
      * Fetch all messages that have not been received
      * Primarily used for debugging
      * TODO temp
      */
     fun allUnReceived(): List<EntityMessage> {
+        Timber.v("Fetching all unreceived messages...")
         return msgRepo.getAllUnreceived()
     }
-
-    /**
-     * Start server connection
-     */
-    fun startServerConnection() {
-        viewModelScope.launch(Dispatchers.IO) {
-            Server.instance()
-        }
-    }
-
     /**
      * Delete a chat by name which is
      * obtained from the list item
      * in the recyclerview
      */
     fun deleteByName(name: String) {
-        viewModelScope.launch {
-            // TODO also provide a variable here...or maybe just use Dispatchers.IO?
-            withContext(CoroutineContextSource().io) {
-                repo.deleteRow(name)
-            }
+        viewModelIO {
+            Timber.v("Call made to delete chat \"$name\"")
+            repo.deleteRow(name)
         }
     }
-
     /**
      * Return all chats from database wrapped
      * in LiveData
      * TODO temp
      */
     fun getAllChatLocalData(): LiveData<List<EntityChat>> {
-        Timber.d("Returning chat live data from VM (calling repo)")
+        Timber.v("Call made to fetch all chats wrapped in live data...")
         return repo.chats()
     }
-
     /**
      * Return all messages from database wrapped
      * in LiveData
      * TODO temp
      */
     fun getLatestMessaageLocalData(): LiveData<EntityMessage> {
-        Timber.d("Returning latest message live data from VM (calling repo)")
+        Timber.v("Call made to get latest message received wrapped in live data...")
         return msgRepo.latestMessage()
     }
-
     /**
      * Update chat item's description when a new
      * message arrives that belongs to it
      */
     fun updateChatWithNewMessage(message: EntityMessage) {
-        Timber.d("OK, i am going to update the chat item this message: \"${message.body}\"...")
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                /**
-                 * Updating the message's received value here to allow
-                 * the UI to display it, because there is a bug that
-                 * displays the latest message on each launch as if
-                 * it is a new message, thus a received field was added
-                 * and a check put in place, hence why the received value
-                 * is set to false when the [ConnectionListener] receives it
-                 */
-                Timber.d("Updating message received field: ${message.body} and ${message.fromName}")
-                val messageUpdate = UpdatedReceived(message.id, true, message.counter)
-                msgRepo.updateReceivedValue(messageUpdate)
-                /**
-                 * Fetch chat id using the sender's name which is not ideal but
-                 * it'll do for now. Then the id is used to update the chat with
-                 * a partial entity object which notifies the view model of data that
-                 * changed and in turn the recyclerview's data gets refreshed with
-                 * the latest data
-                 */
-                val chatId = repo.getChatIdOf(message.fromName)
-                Timber.d("Id of chat to be updated: $chatId with message ${message.body}")
-                repo.updateDescriptionOf(chatId, message.body)
-            }
+        Timber.v("Call made to update chat item description with this message: \"${message.body}\"...")
+        viewModelIO {
+            /**
+             * Updating the message's received value here to allow
+             * the UI to display it, because there is a bug that
+             * displays the latest message on each launch as if
+             * it is a new message, thus a received field was added
+             * and a check put in place, hence why the received value
+             * is set to false when the [ConnectionListener] receives it
+             */
+            Timber.v("Updating message received field: ${message.body} and ${message.fromName}")
+            val messageUpdate = UpdatedReceived(message.id, true, message.counter)
+            msgRepo.updateReceivedValue(messageUpdate)
+            /**
+             * Fetch chat id using the sender's name which is not ideal but
+             * it'll do for now. Then the id is used to update the chat with
+             * a partial entity object which notifies the view model of data that
+             * changed and in turn the recyclerview's data gets refreshed with
+             * the latest data
+             */
+            val chatId = repo.getChatIdOf(message.fromName)
+            Timber.v("Id of chat to be updated: $chatId with message ${message.body}")
+            repo.updateDescriptionOf(chatId, message.body)
         }
     }
-
     /**
      * Update all chats with their new message unreceived counter
      */
     fun updateChatsUnreceivedCounter(data: List<EntityMessage>) {
-        Timber.d("Going to update the chats with their unreceived message counter...")
+        Timber.v("Going to update the chats with their unreceived message counter...")
         viewModelIO {
             val chats = repo.chats.invoke()
             chats.forEach {chat ->
                 val counter = data.count { it.fromName == chat.chatName }
-                Timber.d("unreceived for chat: $chat = ${data.count { it.fromName == chat.chatName }}")
                 repo.updateUnReadOfChat(chat.id, counter)
             }
         }
-    }
-
-    /**
-     * Display the amount of randomly generated chats
-     * that is suppose to exist in the database
-     * TODO temp
-     */
-    private fun randomRecordsCount() {
-        Timber.d("RANDOM RECORDS COUNT: ${addedRecords.size}")
-    }
-
-    /**
-     * Fetch all chats from databse to be
-     * displayed to confirm autogenerated
-     * ID field
-     * TODO temp
-     */
-    fun localRecords() {
-        randomRecordsCount()
-        Timber.d("Viewmodel wants to fetch all the chats from local storage...")
-        viewModelScope.launch(Dispatchers.IO) {
-            val data = repo.justChats()
-            Timber.d("Local chats from database:")
-            data.forEach {
-                Timber.d("[id=${it.id}, name=${it.chatName}]")
-            }
-        }
-    }
-
-    /**
-     * Delete a record in local storage from an
-     * arraylist of randomly generated records
-     * that have been entered into local storage
-     * TODO temp
-     */
-    fun deleteRecord() {
-        val random = Random().nextInt(addedRecords.size)
-        Timber.d("Random int: $random")
-        val toBeRemoved = addedRecords.get(random)
-        addedRecords.remove(toBeRemoved)
-        randomRecordsCount()
-        // TODO replace rest of viewmodelscope.launch with this extension function
-        viewModelIO {
-            repo.deleteRow(toBeRemoved.chatName)
-        }
-    }
-
-    /**
-     * Create a chat entity to be added to local
-     * database to confirm recyclerview data is
-     * updated when data changes
-     * TODO temp
-     */
-    fun insertRandomChat() {
-        val s = EntityChat(
-            chatName = "Person ${Random().nextInt(9999)}",
-            lastActivity = "las",
-            mute = false,
-            description = "def desc",
-            creator = "cre",
-            createdAt = "cred at",
-            subType = "subty",
-            bareJid = "jidd",
-            approved = true,
-            subPending = false,
-            commonGroups = "no groups yo"
-        )
-        addedRecords.add(s) // saved to reference when wanting to delete a list item
-        randomRecordsCount()
-        Timber.d("insert this: $s")
-        viewModelScope.launch (Dispatchers.IO){
-            repo.insert(
-                s
-            )
-        }
-    }
-
-    /**
-     * Build and send a message stanza
-     * TODO temp
-     */
-    fun sendMsgStanza(content: String, to: String) {
-//        val msgStanza = Message(
-//            JidCreate.bareFrom("${to}@jabber-hosting.de"),
-//            "$content"
-//        )
-//        msgStanza.type = Message.Type.chat
-//        Server.instance().sendStanza(msgStanza)
-//        if(!::chat.isInitialized) {
-//            if(!::chatMan.isInitialized) {
-//                chatMan = ChatManager.getInstanceFor(Server.instance())
-//                chatMan.addIncomingListener(ConnectionListener())
-//            }
-//            chat = chatMan.chatWith(JidCreate.entityBareFrom("${to}@jabber-hosting.de"))
-//        }
-//        chat.send("$content")
     }
 }
